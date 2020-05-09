28bae831d9ac1fd4170129ca4f0323c6bd34cc26
Alexandr Moroz <afmoroz@gmail.com>
2020-05-09 16:06:49 +0300

    Updates

---
+++ precache-manifest.2105ead9249f3efbe0f57d8d055e027e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4875088ed0f3be3e16775d2d8e179f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43eda5d401f2b7a2d44b",
+    "url": "/static/css/main.fe5abe27.chunk.css"
+  },
+  {
+    "revision": "ed8cdd121427c2908887",
+    "url": "/static/js/2.4d7b9d91.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.4d7b9d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43eda5d401f2b7a2d44b",
+    "url": "/static/js/main.2eb5cbe2.chunk.js"
+  },
+  {
+    "revision": "e8f83cce166cebc44d20",
+    "url": "/static/js/runtime-main.e8bea482.js"
+  }
+]);
